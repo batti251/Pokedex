06ad7e2aff88162e8cb5834819a8fc35498d7fb9
Sebastian Bünz <bastibuenz25@gmail.com>
2025-04-02 22:22:35 +0200

    added card-overlay ; prepared PokeURL for more PokeInfo

--- script.js
+++ script.js
@@ -1,54 +1,56 @@
-const POKE_URL = "https://pokeapi.co/api/v2/"
-const POKEPIC_URL = "https://pokeapi.co/api/v2/pokemon/"
+const POKE_URL = "https://pokeapi.co/api/v2/";
+const POKEPIC_URL = "https://pokeapi.co/api/v2/pokemon/";
 
 function init() {
-    getPokeAPI("pokemon",0,12)
+  getPokeAPI("pokemon", 0, 12);
+
 }
 
-async function getPokeAPI(path="",offset="",limit=""){
-    const response = await fetch(POKE_URL+path+"/?offset="+offset+"&limit="+limit)
-    let responseRef = await response.json()
-    let PokeAPI = responseRef.results;
-    let mainContent = document.getElementById('main')
-    loopPokemon(PokeAPI, mainContent)    
+async function getPokeAPI(path = "", offset = "", limit = "") {
+  const response = await fetch(
+    POKE_URL + path + "/?offset=" + offset + "&limit=" + limit
+  );
+  let responseRef = await response.json();
+  let PokeAPI = responseRef.results;
+  let mainContent = document.getElementById("main");
+  loopPokemon(PokeAPI, mainContent);
 }
 
-function loopPokemon(PokeAPI, mainContent) {
-    console.log(PokeAPI);
+async function loopPokemon(PokeAPI, mainContent) {
+  for (let PokeIndex = 0; PokeIndex < PokeAPI.length; PokeIndex++) {
+    let element = PokeAPI[PokeIndex];
+    let PokemonID = element.url.split("/").slice(-2, -1);
+    let PokemonInfoSource = await fetch(element.url);
+    let newSource = await PokemonInfoSource.json();
+    console.log(newSource);
     
-        for (let PokeIndex = 0; PokeIndex < PokeAPI.length; PokeIndex++) {
-        let element = PokeAPI[PokeIndex];
-        let PokemonID = element.url.split("/").slice(-2,-1)
-        mainContent.innerHTML += PokedexTemplate(element, PokemonID)
-    }
+    mainContent.innerHTML += PokedexTemplate(
+      element,
+      PokemonID,
+      PokemonInfoSource
+    );
+  }
 }
 
-
-/* "load more"-Button
-    load next 20 cards /?offset=0&limit=x+${20}
-        async function getPokeAPI + new parameter?   
-
-        Ich muss quasi das offset immer um den Faktor X (hier 12) kumulieren
-        vllt forEach Klick um 12 erhöhen, speichern?
-*/
-function loadMorePokemon() {
-    let button = document.getElementById('loadMoreButton')
-    console.log();
-    let start = 12
-    
-    getPokeAPI("pokemon",12,12)
-    getPokeAPI("pokemon",24,12)
+let start = 12;
+async function loadMorePokemon() {
+  let Button = document.getElementById("loadMoreButton");
+  Button.disabled = true;
+  await getPokeAPI("pokemon", start, 12);
+  start += 12;
+  Button.disabled = false;
 }
-
-
 
 /* get PokemonCards
     get specific url for pokemon v2/pokemon/1  => for Bulbasaur
     onclick overlay PokemonCard
-        get several Info in this Card
+        get several Info in this Card€
             which Info? 
                 - base stats + 
                 - generation encounter?
                 - 
             */
-
+function openPokeCard(x, PokemonID) {
+  let PokeCard = document.getElementById("main");
+  PokeCard.outerHTML += pokeCardTemplate(PokemonID);
+}
--- scripts/template.js
+++ scripts/template.js
@@ -1,11 +1,40 @@
 function PokedexTemplate(element, PokemonID) {
 
   return `
-  <div class="col-2">
+  <div class="col-2" onclick="openPokeCard(this,${PokemonID})">
         <div class="card " style="width: 18rem;">
           <h5 class="card-title">${element.name}</h5>
             <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/${PokemonID}.png" class="card-img-top" alt="...">
         </div>
         </div>
       `;
+}
+
+function pokeCardTemplate(PokemonID) {
+  return `
+  <div class="position-absolute top-50 start-50 translate-middle w-100 p-3 h-100">
+  <div class="card position-absolute top-50 start-50 translate-middle ">
+  <div class="card-header">
+              <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/${PokemonID}.png" class="card-img-top" alt="...">
+              <button type="button" class="btn-close" aria-label="Close"></button>
+    <ul class="nav nav-tabs card-header-tabs">
+      <li class="nav-item">
+        <a class="nav-link active" aria-current="true" href="#">Active</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">Link</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link disabled" aria-disabled="true">Disabled</a>
+      </li>
+    </ul>
+  </div>
+  <div class="card-body">
+    <h5 class="card-title">Special title treatment</h5>
+    <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
+    <a href="#" class="btn btn-primary">Go somewhere</a>
+  </div>
+</div>
+</div>
+  `
 }
