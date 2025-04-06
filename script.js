a69d0ccc771e73c9b80b300be8a30220c7baf21a
Sebastian Bünz <bastibuenz25@gmail.com>
2025-04-06 16:02:01 +0200

    implemented Promise.all for all async functions. mapped via PokeID. loaded type-sprites via API, not ideal yet

--- script.js
+++ script.js
@@ -1,114 +1,210 @@
 const POKE_URL = "https://pokeapi.co/api/v2/";
 const POKEPIC_URL = "https://pokeapi.co/api/v2/pokemon/";
+const POKETYPE_URL = "https://pokeapi.co/api/v2/type/"
 let PokemonID = [];
-let PokemonIDRef = "";
-let string = "";
 
 function init() {
-  getPokeAPI("pokemon", 0, 3);
+  getPokeAPI("pokemon", 0, 20);
 }
 
 /* fetch für Pokemon ID */
 async function getPokeAPI(path = "", offset = "", limit = "") {
-  const response = await fetch(POKE_URL + path + "/?offset=" + offset + "&limit=" + limit);
+  let PokemonIDRef = "";
+  let string = "";
+  const response = await fetch(
+    POKE_URL + path + "/?offset=" + offset + "&limit=" + limit
+  );
   let responseRef = await response.json();
   let PokeAPI = responseRef.results;
   for (let index = 0; index < PokeAPI.length; index++) {
     PokemonIDRef = PokeAPI[index].url.split("/").splice(-2, 1);
     string = PokemonIDRef.toString();
     PokemonID.push(string);
+
   }
-  console.log(PokemonID);
-  await getPokeTypes(PokemonID);
-  await getPokeStats(PokemonID); 
-  await getPokeWeights(PokemonID);
-  await getPokeSprite(PokemonID)
-  await getPokeAbilities(PokemonID);
-   /*getPokeMoves(PokemonID); */
+
+
+ // console.log(PokeAPI); 
+
+  /*   initPokeTemplate(PokemonID); */
+  loadDataReturns();
+
 }
 
-async function getPromise(params) {
-  
+async function loadDataReturns() {
+  const [types, stats, weights, sprites, abilities,names] = await Promise.all([
+    getPokeTypes(PokemonID),
+    getPokeStats(PokemonID),
+    getPokeWeights(PokemonID),
+    getPokeSprite(PokemonID),
+    getPokeAbilities(PokemonID),
+    getPokeNames(PokemonID)
+
+  ]);
+/*   console.log(names);
+  console.log(types),
+  console.log(stats),
+  console.log(weights),
+  console.log(sprites),
+  console.log(abilities) */
+  getData(types, stats, weights, sprites, abilities,names)
+}
+
+function getData(types, stats, weights, sprites, abilities,names){
+  const combinedeData = PokemonID.map(id =>{
+    const type = types.find(type => type.id === id);
+    const stat = stats.find(stat => stat.id === id);
+    const weight = weights.find(weight => weight.id === id);
+    const sprite = sprites.find(sprite => sprite.id === id);
+    const ability = abilities.find(ability => ability.id === id);
+    const name = names.find(name => name.id === id)
+    return {
+      id,
+      name,
+      type,
+      stat,
+      weight,
+      sprite,
+      ability
+    }
+  })
+  renderTemplate(combinedeData)
+
+}
+
+function renderTemplate(combinedeData){
+  let pokeList = document.getElementById('main')
+  pokeList.innerHTML = ""
+  for (let index = 0; index < combinedeData.length; index++) {
+    const element = combinedeData[index];
+    pokeList.innerHTML += PokedexTemplate(element)
+  }
+}
+
+async function getPokeNames(PokemonID) {
+  let nameArray = [];
+  for (let index = 0; index < PokemonID.length; index++) {
+    const element = PokemonID[index];
+    const response = await fetch(POKEPIC_URL + element);
+    let responseRef = await response.json();
+    let pokeName = {
+      id: element,
+      name: responseRef.name,
+    };
+    nameArray.push(pokeName);
+  }
+  return nameArray;
+}
+
+
+async function getPokeStats(PokemonID) {
+  let statsArray = [];
+  for (let index = 0; index < PokemonID.length; index++) {
+    const element = PokemonID[index];
+    const response = await fetch(POKEPIC_URL + element);
+    let responseRef = await response.json();
+    let PokeStats = {
+      id: element,
+      stats: {
+        hp: responseRef.stats[0].base_stat,
+        attack: responseRef.stats[1].base_stat,
+        defense: responseRef.stats[2].base_stat,
+        "special-defense": responseRef.stats[3].base_stat,
+        "special-attack": responseRef.stats[4].base_stat,
+        speed: responseRef.stats[5].base_stat,
+      },
+    };
+    statsArray.push(PokeStats);
+  }
+  return statsArray;
+}
+
+async function getPokeWeights(PokemonID) {
+  let weights = [];
+  for (let index = 0; index < PokemonID.length; index++) {
+    const element = PokemonID[index];
+    const response = await fetch(POKEPIC_URL + element);
+    let responseRef = await response.json();
+    let pokeWeight = {
+      id: element,
+      weight: responseRef.weight,
+    };
+    weights.push(pokeWeight);
+  }
+  return weights;
+}
+
+async function getPokeSprite(PokemonID) {
+  let sprites = [];
+  for (let index = 0; index < PokemonID.length; index++) {
+    const element = PokemonID[index];
+    const response = await fetch(POKEPIC_URL + element);
+    let responseRef = await response.json();
+    let PokeSprite = {
+      id: element,
+      link:
+        "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/" +
+        element +
+        ".png",
+    };
+    sprites.push(PokeSprite);
+  }
+  return sprites;
 }
 
 async function getPokeTypes(PokemonID) {
+  let typeArray = [];
   for (let index = 0; index < PokemonID.length; index++) {
     const element = PokemonID[index];
     const response = await fetch(POKEPIC_URL + element);
     let responseRef = await response.json();
-    console.log(responseRef);
     let PokeTypes = {
-      "id": element,
-      "types": {
-      "type1": responseRef.types[0].type.name,
-      "type2": responseRef.types[1].type.name}
+      id: element,
+      types: {
+        type1: responseRef.types[0].type.url,
+        type2: responseRef.types[1]? responseRef.types[1].type.url : responseRef.types[0].type.url,
+      },
     };
-    console.log(PokeTypes);
-    }
+    const response1 = await fetch(PokeTypes.types.type1);
+    let responseRef1 = await response1.json();
+    const response2 = await fetch(PokeTypes.types.type2);
+    let responseRef2 = await response2.json();
+
+   PokeTypes = {
+    id: element,
+    types: {
+      type1: responseRef1.sprites["generation-vii"]["lets-go-pikachu-lets-go-eevee"].name_icon,
+      type2: responseRef2.sprites["generation-vii"]["lets-go-pikachu-lets-go-eevee"].name_icon
+    },
+  };
+    typeArray.push(PokeTypes);
+
+  }
+  return typeArray;
 }
 
- async function getPokeStats(PokemonID) {
-  for (let index = 0; index < PokemonID.length; index++) {
-    const element = PokemonID[index];
-    const response = await fetch(POKEPIC_URL + element);
-    let responseRef = await response.json();
-    let PokeStats = {
-      "id": element,
-      "stats":{
-      "hp": responseRef.stats[0].base_stat,
-      "attack": responseRef.stats[1].base_stat,
-      "defense": responseRef.stats[2].base_stat,
-      "special-defense": responseRef.stats[3].base_stat,
-      "special-attack": responseRef.stats[4].base_stat,
-      "speed": responseRef.stats[5].base_stat}
-    };
-    console.log(PokeStats);
-    }
-} 
 
 
-async function getPokeWeights(PokemonID) {
-  for (let index = 0; index < PokemonID.length; index++) {
-    const element = PokemonID[index];
-    const response = await fetch(POKEPIC_URL + element);
-    let responseRef = await response.json();
-    let PokeStats = {
-      "id": element,
-      "weight": responseRef.weight
-    };
-    console.log(PokeStats);
-    }
-} 
-
-
-
-async function getPokeSprite(PokemonID) {
-  for (let index = 0; index < PokemonID.length; index++) {
-    const element = PokemonID[index];
-    const response = await fetch(POKEPIC_URL + element);
-    let responseRef = await response.json();
-    let PokeSprite = "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/"+element+".png"
-    console.log(PokeSprite);
-    }
-} 
 
 async function getPokeAbilities(PokemonID) {
+  let abilities = [];
   for (let index = 0; index < PokemonID.length; index++) {
     const element = PokemonID[index];
     const response = await fetch(POKEPIC_URL + element);
     let responseRef = await response.json();
     let PokeAbilities = {
-      "id": element,
-      "ability1": responseRef.abilities[0].ability.name,
-      "ability2": responseRef.abilities[1].ability.name
+      id: element,
+      ability1: responseRef.abilities[0].ability.name,
+      ability2: responseRef.abilities[1] ? responseRef.abilities[1].ability.name :null ,
     };
-    console.log(PokeAbilities);
-    }
-} 
-
+    abilities.push(PokeAbilities);
+  }
+  return abilities;
+}
 
 let start = 20;
 async function loadMorePokemon() {
+
   let Button = document.getElementById("loadMoreButton");
   Button.disabled = true;
   await getPokeAPI("pokemon", start, 20);
@@ -126,10 +222,10 @@ async function loadMorePokemon() {
                 - 
             */
 
-/* Close Pokecard */
-function openPokeCard(x) {
-  let PokeCard = document.getElementById("main");
-  PokeCard.outerHTML += pokeCardTemplate();
+/* open Pokecard */
+function openPokeCard(x,sprite) {
+let focusCard = document.getElementById('focus-card')
+focusCard.innerHTML += pokeCardTemplate(sprite)
 }
 
 /* URL für Type */
--- scripts/template.js
+++ scripts/template.js
@@ -1,51 +1,81 @@
-function PokedexTemplate(PokeTypes) {
-
-console.log(PokeTypes);
-
-
-
-
-
+function PokedexTemplate(element) {
+  //console.log(element);
+  
   return `
-  <div class="col-2" onclick="openPokeCard(this,)">
-        <div class="card " style="width: 18rem;">
-          <h5 class="card-title"></h5>
-            <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/.png" class="card-img-top" alt="...">
-             <div class="card-body">
-    <p class="card-text">
-     <img src="" class="card-img-top" alt="...">
-    </p>
+
+<!-- Button trigger modal -->
+<div type="button" class="col-2 card menu" data-bs-toggle="modal" data-bs-target="#${element.id}exampleModal">
+<div class="card-body">
+    <h5 class="card-title">${element.name.name}</h5>
+</div>
+    <img src="${element.sprite.link}" class="card-img-top" alt="Pokemon Sprites">
+
+<div class="row">
+  <div class="col-sm-6 mb-3 mb-sm-0">
+        <p class="img"> <img src="${element.type.types.type1}" class="card-img" alt="Pokemon Type 1"></p>
   </div>
+  <div class="col-sm-6">
+        <p class="img"> <img src="${element.type.types.type2}" class="card-img" alt="Pokemon Type 2"></p>
+
+  </div>
+</div>
+
+  </div>
+  
+
+    <!-- Modal -->
+  <div class="modal fade" id="${element.id}exampleModal" tabindex="-1" aria-labelledby="${element.id}exampleModalLabel" aria-hidden="false">
+    <div class="modal-dialog modal-dialog-scrollable">
+      <div class="modal-content">
+        <div class="modal-header">
+          <h1 class="modal-title fs-5" id="${element.id}exampleModalLabel">Modal title</h1>
+          <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
         </div>
+        <div class="modal-body">
+<div class="card" style="width: 29rem !important;">
+  <img src="${element.sprite.link}" class="card-img" alt="Pokemon Sprite">
+  <div class="card-body">
+    <h5 class="card-title">${element.name.name}</h5>
+    <p class="card-text">${element.ability.ability1}</p>
+    <p class="card-text">${element.ability.ability2}</p>
+  </div>
+  <ul class="list-group list-group-flush">
+    <li class="list-group-item">An item</li>
+    <li class="list-group-item">Weight: ${element.weight.weight}</li>
+    <li class="list-group-item">A third item</li>
+  </ul>
+  <div class="card-body">
+    <a href="#" class="card-link">Card link</a>
+    <a href="#" class="card-link">Another link</a>
+  </div>
+</div>
         </div>
+        <div class="modal-footer">
+          <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+        </div>
+      </div>
+    </div>
+  </div>
       `;
 }
 
-function pokeCardTemplate() {
+
+
+
+/* 
+function PokedexTemplate(element) {
   return `
-  <div class="position-absolute top-50 start-50 translate-middle w-100 p-3 h-100">
-  <div class="card position-absolute top-50 start-50 translate-middle ">
-  <div class="card-header">
-              <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/.png" class="card-img-top" alt="...">
-              <button type="button" class="btn-close" aria-label="Close"></button>
-    <ul class="nav nav-tabs card-header-tabs">
-      <li class="nav-item">
-        <a class="nav-link active" aria-current="true" href="#">Active</a>
-      </li>
-      <li class="nav-item">
-        <a class="nav-link" href="#">Link</a>
-      </li>
-      <li class="nav-item">
-        <a class="nav-link disabled" aria-disabled="true">Disabled</a>
-      </li>
-    </ul>
-  </div>
-  <div class="card-body">
-    <h5 class="card-title">Special title treatment</h5>
-    <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
-    <a href="#" class="btn btn-primary">Go somewhere</a>
-  </div>
-</div>
-</div>
-  `
-}
+  <div  class="col-2 " onclick="openPokeCard(this,'${element.sprite.link}')">
+   <div class="card menu" style="width: 18rem;">
+          <h5 class="card-title">${element.name.name}</h5>
+            <img src=${element.sprite.link} class="card-img-top" alt="...">
+
+    <p class="card-text">
+     <img src="" class="card-img-top" alt="...">${element.type.types.type1}
+     <img src="" class="card-img-top" alt="...">${element.type.types.type2}
+    </p>
+
+    </div>
+        </div>
+      `;
+} */
